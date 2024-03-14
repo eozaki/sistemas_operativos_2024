2914838e26e1ba0adff5a5790507c5290f43c38f
Erick Cordeiro Ozaki <erick@MacBook-Pro-de-Erick.local>
2024-03-14 19:07:15 +0000

    Aula 5 - ex 4

--- aula05/exemplo-3.1.c
+++ aula05/exemplo-3.1.c
@@ -13,4 +13,23 @@ int main() {
     b = so_getf();
 
     printf("%d + %f = %f\n", a, b, a + b );
+
+    // Ex 4.a
+    printf("Escreva um número inteiro: ");
+    a = so_geti();
+    printf("Escreva um número com vírgula flutuante: ");
+
+    b = so_getf();
+    float c = (float) a;
+    c > b ? printf("%f\n", c) : printf("%f\n", b);
+
+    // Ex 4.b
+    printf("Escreva um número inteiro: ");
+    a = so_geti();
+    printf("Escreva um número com vírgula flutuante: ");
+
+    b = so_getf();
+    c = (float) a;
+    c > b ? printf("%f, %f\n", b, c) : printf("%f, %f\n", c, b);
+
 }
