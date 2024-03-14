16b301f8d65c63bf454da4ba8bbe1d998297c49c
Erick Cordeiro Ozaki <erick@MacBook-Pro-de-Erick.local>
2024-03-14 18:49:40 +0000

    Aula05 - ex 3

--- aula05/exemplo-3.1.c
+++ aula05/exemplo-3.1.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "so_utils.h"
+
 int main() {
     int a;
     float b;
+    char buffer[10 + 1]; // 10 caracteres no maximo para leitura do valor, mais um char para o \n obrigatorio
+    printf("Agora com fgets...\n");
+
     printf("Escreva um número inteiro: ");
-    scanf("%d", &a);
+    fgets(buffer, 11, stdin);
+    a = atoi(buffer);
     printf("Escreva um número com vírgula flutuante: ");
-    scanf("%f", &b);
+    fgets(buffer, 11, stdin);
+    b = atof(buffer);
+
     printf("%d + %f = %f\n", a, b, a + b );
 }
